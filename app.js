d1da3c183a01174aa7d94c4b676f09954ed245cd
Sebastián Figueroa <sebastian.figueroa@parqueexplora.org>
2021-04-25 09:55:16 -0500

    Level 1 Security implemented

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose")
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User",userSchema);
+
+
+
+
+app.get("/", (req,res) => {
+    res.render("home");
+})
+
+app.get("/login", (req,res) => {
+    res.render("login");
+})
+
+app.post("/login",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username},(err,user)=>{
+        if(user){
+            if (user.password === password){
+                res.render("secrets");
+            }
+        }
+    })
+})
+
+app.get("/register", (req,res) => {
+    res.render("register");
+})
+
+app.post("/register",(req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("secrets");
+        };
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("Listening in port 3000");
+});         
